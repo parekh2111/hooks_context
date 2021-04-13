b21eb8a08ea4072a3b92deb6b65a62a5c56328b4
parekh2111 <parekh2111@gmail.com>
2021-04-13 16:18:49 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,14 +1,9 @@
+// @ts-nocheck
 import React, { useReducer } from 'react';
 import axios from 'axios';
 import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
-import {
-  SEARCH_USERS,
-  SET_LOADING,
-  CLEAR_USERS,
-  GET_USER,
-  GET_REPOS
-} from '../types';
+import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../types';
 
 let githubClientId;
 let githubClientSecret;
